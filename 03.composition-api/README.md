25f3b187d5bf40a4c6d70c0e123b3b13fb7c6ac3
kiosk123 <heo9910@naver.com>
2022-07-06 23:31:07 +0900

    .

--- 03.composition-api/README.md
+++ 03.composition-api/README.md
@@ -58,3 +58,51 @@ export default {
   },
 }
 ```
+
+## 템플릿 문법
+```vue
+<template>
+  <div v-once>{{ message }}</div> <!-- v-once 한번만 출력-->
+  <button @click="add">add !</button>
+  <p v-html="rawHtml"></p> <!-- v-html은 html엘리먼트 그대로 출력-->
+  <h2>속성 바인딩</h2>
+  <div :title="dynamicTitle">마우스를 올려보세요</div>
+
+  <!-- 객체를 이용한 속성 바인딩-->
+  <input v-bind="attrs" />
+
+  <!-- 템플릿 안에 자바스크립트 함수도 사용가능 -->
+  <div>{{ [...message].reverse().join('') }}</div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const message = ref('안녕하세요')
+    const rawHtml = ref('<strong>안녕하세요</strong>')
+    const dynamicTitle = ref('안녕하세요!!!!')
+    const attrs = ref({
+      typs: 'password',
+      value: '12345678',
+      disabled: false,
+    })
+
+    const add = () => {
+      message.value = message.value + '!'
+    }
+
+    return {
+      message,
+      add,
+      rawHtml,
+      dynamicTitle,
+      attrs,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
+```
--- 03.composition-api/learn-vue3/src/App.vue
+++ 03.composition-api/learn-vue3/src/App.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>{{ counter }}</div>
-  <button @click="addCounter">Increment</button>
+  <div v-once>{{ message }}</div>
+  <button @click="add">add !</button>
+  <p v-html="rawHtml"></p>
+  <h2>속성 바인딩</h2>
+  <div :title="dynamicTitle">마우스를 올려보세요</div>
+
+  <!-- 객체를 이용한 속성 바인딩-->
+  <input v-bind="attrs" />
+
+  <!-- 템플릿 안에 자바스크립트 함수도 사용가능 -->
+  <div>{{ [...message].reverse().join('') }}</div>
 </template>
 
 <script>
@@ -8,14 +17,25 @@ import { ref } from 'vue'
 
 export default {
   setup() {
-    const counter = ref(0)
-    const addCounter = () => {
-      counter.value++
+    const message = ref('안녕하세요')
+    const rawHtml = ref('<strong>안녕하세요</strong>')
+    const dynamicTitle = ref('안녕하세요!!!!')
+    const attrs = ref({
+      typs: 'password',
+      value: '12345678',
+      disabled: false,
+    })
+
+    const add = () => {
+      message.value = message.value + '!'
     }
 
     return {
-      counter,
-      addCounter,
+      message,
+      add,
+      rawHtml,
+      dynamicTitle,
+      attrs,
     }
   },
 }
